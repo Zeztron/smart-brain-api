69e43359f638e704ac1e9f10098031a06e6595e3
Zeztron <pateltharsh@gmail.com>
2018-12-01 18:09:12 -0500

    Fixed more bugs whats new

--- server.js
+++ server.js
@@ -78,9 +78,9 @@ app.get("/profile/:id", (req, res) => {
 });
 
 app.put("/image", (req, res) => {
-    const { id } = req.params;
+    const { id } = req.body;
     let found = false;
-    database.users.forEach(users => {
+    database.users.forEach(user => {
         if (user.id == id) {
             found = true;
             user.entries++;
