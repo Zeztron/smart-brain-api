54bece1a9aeeb8951f34b8266908075a20bcbe66
Zeztron <pateltharsh@gmail.com>
2018-12-02 18:18:08 -0500

    Finished registering the user

--- server.js
+++ server.js
@@ -64,12 +64,17 @@ app.post("/register", (req, res) => {
     bcrypt.hash(password, null, null, function (err, hash) {
         console.log(hash);
     });
-    db("users").insert({
-        email: email,
-        name: name,
-        joined: new Date()
-    }).then(console.log)
-    res.json(database.users[database.users.length - 1]);
+    db("users")
+        .returning("*")
+        .insert({
+            email: email,
+            name: name,
+            joined: new Date()
+        })
+        .then(user => {
+            res.json(user[0]);
+        })
+        .catch(err => res.status(400).json("Unable to Register"))
 });
 
 app.get("/profile/:id", (req, res) => {
